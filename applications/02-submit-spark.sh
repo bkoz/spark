14f494bf0ce7d2c1d9f24576215748c2a27eac10
Bob Kozdemba <bkozdemba@gmail.com>
2023-08-14 19:49:06 +0000

    Added python arg

--- applications/02-submit-spark.sh
+++ applications/02-submit-spark.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-MASTER=$(oc whoami --show-server)
+export MASTER=$(oc whoami --show-server)
 echo "MASTER = ${MASTER}"
-
 
 spark-submit \
     --verbose \
@@ -15,15 +14,4 @@ spark-submit \
     --conf spark.kubernetes.authenticate.executor.serviceAccountName=spark-service-account \
     --conf spark.kubernetes.namespace=bkoz-devspaces \
     --conf spark.kubernetes.container.image=gcr.io/spark-operator/spark-py:v3.1.1 \
-    local:///opt/spark/examples/src/main/python/pi.py
-
-#$ ./bin/spark-submit \
-#    --master k8s://https://<k8s-apiserver-host>:<k8s-apiserver-port> \
-#    --deploy-mode cluster \
-#    --name spark-pi \
-#    --class org.apache.spark.examples.SparkPi \
-#    --conf spark.executor.instances=5 \
-#    --conf spark.kubernetes.container.image=<spark-image> \
-#    local:///path/to/examples.jar
-
-#    --conf spark.kubernetes.authenticate.driver.serviceAccountName=spark-service-account \
+    local:///opt/spark/examples/src/main/python/pi.py 1000
